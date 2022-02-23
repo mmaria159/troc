85b9b7c29e67d6a4a1b6ebaf7898d677494e9f76
mmaria159 <macovei.maria1999@gmail.com>
2022-02-23 23:48:28 +0200

    Add TrocApplication.class

---
+++ src/main/java/com/troc/TrocApplication.java
@@ -0,0 +1,11 @@
+package com.troc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrocApplication.class, args);
+    }
+}
