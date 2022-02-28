1f69136b54a611115208dadcce7f7bda9fcea1f0
mmaria159 <macovei.maria1999@gmail.com>
2022-02-28 20:33:14 +0200

    Add exceptions

---
+++ src/main/java/com/troc/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.troc.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
